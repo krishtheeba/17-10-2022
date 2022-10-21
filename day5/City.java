b2e84068537100b9aa5407116ffbdc2ee0ff1116
KRISH_THEEBA <59197696+krishtheeba@users.noreply.github.com>
2022-10-22 00:41:20 +0530

    Create City.java

---
+++ day5/City.java
@@ -0,0 +1,20 @@
+package stream;
+
+public class City {
+
+	String city;
+
+	public City(String city) {
+		super();
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+}
